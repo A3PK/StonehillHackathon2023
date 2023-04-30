1c4659ba1f801c12773a446c4e3f606629c9bbbf
tv.de <132119257+Unblender@users.noreply.github.com>
2023-04-30 06:39:52 +0530

    Files for websites

---
+++ website.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  <div style="text-align: center;">
+  <h1><p style="font-size:50px;">Bionic Eye</p>
+  
+  </h1>
+</div>
+    <body style="background-color: #66FFFF;">
+</body>
+    <meta name="description" content="Short product description.">
+    <meta name="keywords" content="product, brand, keywords">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="product-page">
+      <div class="product-image">
+     
+     
+      </div>
+      <div class="product-details">
+  
+
+        <h2>By
+        
+        
+        LookSavvy
+        <p style="font-size: 50px;"></p>
+     </h2>
+
+
+        <div class="product-rating">
+          <span class="star">&#9733;</span>
+          <span class="star">&#9733;</span>
+          <span class="star">&#9733;</span>
+          <span class="star">&#9733;</span>
+          <span class="star">&#9733;</span>
+          <span class ="  rating-count">    (42k)  </span>
+        </div>
+        <div class="product-price">$69,000</div>
+        <div class="product-description">
+         <p style="font-size: 30px;">EYE DESCRIPTION</p>
+       
+          <ul>
+            <li>cures blindness in one eye</li>
+            <li>connect with our smart glasses</li>
+            <li>it is modern and slick</li>
+          </ul>
+        </div>
+        <form action="add-to-cart.php" method="post">
+          <label for="quantity">Quantity:</label>
+          <input type="number" id="quantity" name="quantity" min="1" max="1" value="1">
+          <button type="submit">Buy</button>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
